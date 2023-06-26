543289227bf640a8a967563380338b515f71efba
alejandro <alejandrorecio.r@gmail.com>
2023-06-26 22:40:47 +0200

    Add first account endpoint

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('account')->group(function () {
+    Route::get('/', [AccountController::class, 'get']);
 });
