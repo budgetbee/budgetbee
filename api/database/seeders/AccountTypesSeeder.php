773d227352668e370cdc024e14418ca5a1eb1620
alejandro <alejandrorecio.r@gmail.com>
2023-06-26 18:56:20 +0200

    Create Categories seeder

---
+++ api/database/seeders/AccountTypesSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AccountTypes;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AccountTypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $json = file_get_contents(__DIR__ . '/data/account_types.json');
+        $data = json_decode($json, true);
+
+        
+        foreach ($data as $accountType) {
+            $newAccountType = new AccountTypes();
+            $newAccountType->fill([
+                'name' => $accountType['name']
+            ]);
+            $newAccountType->save();
+        }
+    }
+}
