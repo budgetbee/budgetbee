6fb3049737bb8ce990980c772cc359ee80327803
alejandro <alejandrorecio.r@gmail.com>
2023-06-26 23:29:09 +0200

    Create Authcontroller

---
+++ api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $validatedData = $request->validate([
+            'email' => 'required|string|max:255|email|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        $user = User::create(
+            [
+                'email' => $validatedData['email'],
+                'password' => Hash::make($validatedData['password'])
+            ]
+        );
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request->input('email'))->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+
+}
