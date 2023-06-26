8ed4158cb656c6a5a4a5c19db93434342ce2ce3d
alejandro <alejandrorecio.r@gmail.com>
2023-06-26 23:35:48 +0200

    Add seeders

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -18,5 +18,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call(AccountTypesSeeder::class);
+        $this->call(CategoriesSeeder::class);
     }
 }
