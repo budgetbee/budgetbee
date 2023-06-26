3e30d480f94e16f3c26fd932fb3520199824428e
alejandro <alejandrorecio.r@gmail.com>
2023-06-26 23:35:40 +0200

    Make user_id as optional

--- api/database/migrations/2023_05_05_124600_create_categories_table.php
+++ api/database/migrations/2023_05_05_124600_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->unsignedBigInteger('parent_category_id');
             $table->string('name');
             $table->string('icon');
