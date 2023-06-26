aff7d9fc792937edf83096096e47a32d8d04a10d
alejandro <alejandrorecio.r@gmail.com>
2023-06-26 23:29:19 +0200

    Set name as optional

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
