be464c18bd107c6824282a2ab9a3943ed116d779
alejandro <alejandrorecio.r@gmail.com>
2023-06-26 18:57:25 +0200

    Create accounts migration

---
+++ api/database/migrations/2023_05_05_124600_create_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('name');
+            $table->float('initial_balance')->default(0);
+            $table->float('current_balance')->default(0);
+            $table->unsignedBigInteger('type_id');
+            $table->string('color');
+            $table->foreign('type_id')->references('id')->on('account_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
