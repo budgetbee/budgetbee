dddc8082200674b0818a53affae7e3499c0ed831
alejandro <alejandrorecio.r@gmail.com>
2023-06-26 18:55:51 +0200

    Create Category model

---
+++ api/app/Models/Category.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name', 'icon', 'parent_category_id'
+    ];
+
+    protected $appends = ['color', 'parent_name'];
+
+    protected $hidden = ['parent'];
+
+    public function parent()
+    {
+        return $this->belongsTo(ParentCategory::class, 'parent_category_id', 'id');
+    }
+
+    public function getColorAttribute()
+    {
+        return $this->parent->color;
+    }
+
+    public function getParentNameAttribute()
+    {
+        return $this->parent->name;
+    }
+
+}
