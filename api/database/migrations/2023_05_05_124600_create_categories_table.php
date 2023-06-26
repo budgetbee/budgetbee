63bb5348dc5f941a1278a9e14cff6c9efd79a1b2
alejandro <alejandrorecio.r@gmail.com>
2023-06-26 18:57:32 +0200

    Create categories migration

---
+++ api/database/migrations/2023_05_05_124600_create_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('parent_category_id');
+            $table->string('name');
+            $table->string('icon');
+            $table->foreign('parent_category_id')->references('id')->on('parent_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
