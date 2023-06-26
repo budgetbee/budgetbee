490c0f2785015515482cd2019615aa60d6a04cba
alejandro <alejandrorecio.r@gmail.com>
2023-06-27 00:35:54 +0200

    Set 1000 requests per minute

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -40,7 +40,8 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            // \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            'throttle:1000,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
