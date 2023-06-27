acef82f92739bf032549909192b4b32ae6a6eaef
alejandro <alejandrorecio.r@gmail.com>
2023-06-27 22:04:54 +0200

    Change float menu style

--- app/src/layout/FloatMenu.jsx
+++ app/src/layout/FloatMenu.jsx
@@ -1,24 +1,27 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 // Icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-
-import FloatMenuOptions from "./Components/FloatMenuOptions";
 
 export default function FloatMenu() {
     const [isOpen, setIsOpen] = useState(false);
 
     const handleClick = () => {
         setIsOpen(!isOpen);
-    }
+    };
 
     return (
-        <div className="fixed bottom-24 right-5 z-40">
-            {isOpen && <FloatMenuOptions />}
-            <div className="rounded-full bg-blue-500" onClick={handleClick}>
-                <FontAwesomeIcon icon={faPlus} className={"text-white text-4xl p-5"} />
-            </div>
+        <div className="fixed bottom-10 right-10 z-40">
+            <Link to="/record">
+                <div className="flex items-center rounded-full w-20 h-20 bg-[#C3D4E6] drop-shadow" onClick={handleClick}>
+                    <FontAwesomeIcon
+                        icon={faPlus}
+                        className={"text-gray-800 m-auto text-4xl"}
+                    />
+                </div>
+            </Link>
         </div>
     );
 }
