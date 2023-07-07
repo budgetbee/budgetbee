4592a6d0973654624068ca8d9c82e5b9328d29ab
alejandro <alejandrorecio.r@gmail.com>
2023-07-07 22:23:27 +0200

    Accept custom functions

--- web/src/layout/TopNav.jsx
+++ web/src/layout/TopNav.jsx
@@ -1,22 +1,51 @@
 import React from "react";
-
-// Icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
-export default function TopNav() {
-    
+export default function TopNav({
+    leftFunction,
+    leftIcon,
+    rightFunction,
+    rightIcon,
+}) {
+    const handleLeftClick = () => {
+        if (leftFunction) {
+            leftFunction();
+        } else {
+            window.history.back();
+        }
+    };
+
+    const handleRightClick = () => {
+        if (rightFunction) {
+            rightFunction();
+        } else {
+            window.history.back();
+        }
+    };
+
     return (
-        <div className="fixed w-full flex flex-row h-14 justify-left items-center bg-gray-700 z-40">
+        <div className="fixed w-full flex flex-row h-14 justify-between items-center bg-gray-700 z-40">
             <div
                 className="py-3 pl-5 pr-10 cursor-pointer"
-                onClick={() => window.history.back()}
+                onClick={handleLeftClick}
             >
                 <FontAwesomeIcon
-                    icon={faArrowLeft}
-                    className={"text-white text-2xl"}
+                    icon={leftIcon || faArrowLeft}
+                    className="text-white text-2xl"
                 />
             </div>
+            {rightFunction && (
+                <div
+                    className="py-3 pl-5 pr-10 cursor-pointer"
+                    onClick={handleRightClick}
+                >
+                    <FontAwesomeIcon
+                        icon={rightIcon}
+                        className="text-white text-2xl"
+                    />
+                </div>
+            )}
         </div>
     );
 }
