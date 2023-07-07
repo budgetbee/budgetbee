2975b2a148e6402c777de540487a7f10646aa5e9
alejandro <alejandrorecio.r@gmail.com>
2023-07-07 18:25:40 +0200

    Add deletes

--- api/database/migrations/2023_05_05_195451_create_records_table.php
+++ api/database/migrations/2023_05_05_195451_create_records_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('records', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->softDeletes();
             $table->datetime('date');
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('from_account_id');
