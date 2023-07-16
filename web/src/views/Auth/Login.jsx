e1750c64c44d6e9b25f88f5298ca10a4356e18c0
Alejandro <alejandrorecio.r@gmail.com>
2023-07-16 23:59:10 +0200

    Remove remember me and forgot password

--- web/src/views/Auth/Login.jsx
+++ web/src/views/Auth/Login.jsx
@@ -122,33 +122,6 @@ export default function Login() {
                                         required=""
                                     ></input>
                                 </div>
-                                <div className="flex items-center justify-between">
-                                    <div className="flex items-start">
-                                        <div className="flex items-center h-5">
-                                            <input
-                                                id="remember"
-                                                aria-describedby="remember"
-                                                type="checkbox"
-                                                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
-                                                required=""
-                                            ></input>
-                                        </div>
-                                        <div className="ml-3 text-sm">
-                                            <label
-                                                for="remember"
-                                                className="text-gray-500"
-                                            >
-                                                Remember me
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <a
-                                        href="#"
-                                        className="text-sm font-medium text-primary-600 hover:underline"
-                                    >
-                                        Forgot password?
-                                    </a>
-                                </div>
                                 {button}
                             </form>
                         </div>
