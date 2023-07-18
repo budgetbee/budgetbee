642752efd839533af2e6e56dbdf2a1f9230e1e41
Alejandro <alejandrorecio.r@gmail.com>
2023-07-19 00:38:34 +0200

    Check if menu true and show left sidebar

--- web/src/layout/TopNav.jsx
+++ web/src/layout/TopNav.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faBars } from "@fortawesome/free-solid-svg-icons";
+
+import LeftSidebarMenu from "./LeftSidebarMenu";
 
 export default function TopNav({
+    menu = false,
     leftFunction,
     leftIcon,
     rightFunction,
     rightIcon,
 }) {
+    const [openSidebarMenu, setOpenSidebarMenu] = useState(false);
+    const [activePage, setActivePage] = useState("");
+
+    useEffect(() => {
+        // Get the active page from the URL's first parameter
+        const url = window.location.href;
+        const segments = url.split("/");
+        const activePageFromUrl = segments[3]; // Assuming the active page is the 3rd segment of the URL
+
+        setActivePage(activePageFromUrl);
+    }, []);
+
     const handleLeftClick = () => {
-        if (leftFunction) {
+        if (menu) {
+            setOpenSidebarMenu(true);
+        } else if (leftFunction) {
             leftFunction();
         } else {
             window.history.back();
@@ -24,8 +41,17 @@ export default function TopNav({
         }
     };
 
+    if (menu) {
+        leftIcon = faBars;
+    }
+
     return (
-        <div className="fixed w-full flex flex-row h-14 justify-between items-center bg-gray-700 z-40">
+        <div className="fixed w-full top-0 basis-1/12 flex flex-row justify-between items-center bg-gray-700 h-14 z-30">
+            <LeftSidebarMenu
+                open={openSidebarMenu}
+                setOpen={setOpenSidebarMenu}
+                activePage={activePage}
+            />
             <div
                 className="py-3 pl-5 pr-10 cursor-pointer"
                 onClick={handleLeftClick}
@@ -37,7 +63,7 @@ export default function TopNav({
             </div>
             {rightFunction && (
                 <div
-                    className="py-3 pl-5 pr-10 cursor-pointer"
+                    className="py-3 pr-5 pl-10 cursor-pointer"
                     onClick={handleRightClick}
                 >
                     <FontAwesomeIcon
