2f001c8268997a546f3eaf05748416b64c456b31
Alejandro <alejandrorecio.r@gmail.com>
2023-07-19 12:59:24 +0200

    Remove comments

--- api/routes/api.php
+++ api/routes/api.php
@@ -10,15 +10,11 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
 Route::post('/login', [AuthController::class, 'login']);
