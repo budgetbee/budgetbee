14e1782a563a5ff939fe651909eaabea64a09fbe
Alejandro <alejandrorecio.r@gmail.com>
2023-07-24 17:03:55 +0200

    Add some tests

--- api/tests/Feature/CreateRecordTest.php
+++ api/tests/Feature/CreateRecordTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Record;
+use App\Models\Account;
 
 class CreateRecordTest extends TestCase
 {
@@ -86,5 +87,62 @@ class CreateRecordTest extends TestCase
         $response = $this->post('/api/record', $recordData);
 
         $response->assertStatus(302);
+    }
+
+    /**
+     * Create a transfer record
+     */
+    public function testCreateTransferSuccess(): void
+    {
+        $recordData = Record::factory()->make()->toArray();
+        $recordData['type'] = 'transfer';
+        $recordData['to_account_id'] = Account::whereNot('id', $recordData['from_account_id'])->inRandomOrder()->first()->id;
+        $response = $this->post('/api/record', $recordData);
+
+        $response->assertStatus(200);
+        
+        $id = json_decode($response->getContent())->id;
+        $record = Record::find($id);
+
+        $this->assertIsInt($record->link_record_id);
+
+        $assocRecord = Record::find($record->link_record_id);
+
+        $this->assertIsObject($assocRecord);
+        $this->assertEquals($record->id, $assocRecord->link_record_id);
+        $this->assertEquals($record->link_record_id, $assocRecord->id);
+        $this->assertEquals($record->amount, -$assocRecord->amount);
+        $this->assertEquals($record->type, 'transfer');
+
+        $record->delete();
+        $assocRecord->delete();
+    }
+
+    /**
+     * Update a transfer record
+     */
+    public function testUpdateTransferSuccess(): void
+    {
+        $recordData = Record::factory()->make()->toArray();
+        $recordData['type'] = 'transfer';
+        $recordData['to_account_id'] = Account::whereNot('id', $recordData['from_account_id'])->inRandomOrder()->first()->id;
+        $response = $this->post('/api/record', $recordData);
+
+        $response->assertStatus(200);
+
+        $id = json_decode($response->getContent())->id;
+        $record = Record::find($id);
+        
+        $recordData['type'] = 'expense';
+        unset($recordData['to_account_id']);
+        $response = $this->post('/api/record/' . $record->id, $recordData);
+        
+        $response->assertStatus(200);
+
+        $assocRecord = Record::find($record->link_record_id);
+
+        $this->assertNull($assocRecord);
+
+        $record->delete();
     }
 }
