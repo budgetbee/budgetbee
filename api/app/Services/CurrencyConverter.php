38e9284a90c4ce21bc83c3df9a0b51ee4621a88a
Alejandro <alejandrorecio.r@gmail.com>
2023-08-02 21:48:33 +0200

    Check if same currency

--- api/app/Services/CurrencyConverter.php
+++ api/app/Services/CurrencyConverter.php
@@ -8,6 +8,9 @@ class CurrencyConverter
 {
     public static function convert(float $amount, UserCurrency $from)
     {
-        return $from->exchange_rate_to_default_currency * $amount;
+        if ($from->currency->code === $from->user->currency->code) {
+            return $amount;
+        }
+        return $amount / $from->exchange_rate_to_default_currency;
     }
 }
