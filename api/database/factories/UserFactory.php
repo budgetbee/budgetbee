dc7bdef609a285498eccdc6894b7839e6e10ea55
Alejandro <alejandrorecio.r@gmail.com>
2023-08-04 21:09:11 +0200

    Remove currency_id

--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -23,8 +23,7 @@ class UserFactory extends Factory
             'email' => time() . fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-            'currency_id' => UserCurrency::inRandomOrder()->first()->id
+            'remember_token' => Str::random(10)
         ];
     }
 
