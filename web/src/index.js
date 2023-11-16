7d3345caab9bf42d2a579e2999d3524109998689
Alejandro Recio <alejandrorecio.r@gmail.com>
2023-11-16 21:12:41 +0100

    Remove strict mode (#68)

--- web/src/index.js
+++ web/src/index.js
@@ -5,13 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
