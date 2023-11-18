628a4f7ae45d58dcfcb7d2256ba553b4babcf1ae
Gurpreet Singh S <sgsingh98@yahoo.com>
2023-11-18 01:05:35 +0100

    Moving composer install command for development

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ ! -z $APP_ENV ]]; then 
+    echo "App env: " "$APP_ENV"
+    if [[ "$APP_ENV" == "local" ]]; then
+        composer install --no-dev --optimize-autoloader
+    fi
+fi
+
 # Run key:generate
 # php artisan key:generate
 
